35246812e63b547bfc70a66a713833d26abb0d0b
paulinacarrero <paulinacarrero123@gmail.com>
2026-04-05 20:53:21 -0500

    registro rostro javaScript registro del rostro.

---
+++ registroRostro.js
@@ -0,0 +1,35 @@
+function abrirCamara() {
+const video = document.getElementById("video");
+const foto = document.getElementById("foto");
+
+foto.style.display = "none";
+video.style.display = "block";
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+    video.srcObject = stream;
+    })
+    .catch(error => {
+    alert("No se pudo acceder a la cámara");
+    });
+}
+
+function tomarFoto() {
+const video = document.getElementById("video");
+const canvas = document.createElement("canvas");
+const foto = document.getElementById("foto");
+
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+
+const ctx = canvas.getContext("2d");
+ctx.drawImage(video, 0, 0);
+
+foto.src = canvas.toDataURL("image/png");
+
+foto.style.display = "block";
+video.style.display = "none";
+
+const stream = video.srcObject;
+stream.getTracks().forEach(track => track.stop());
+}
